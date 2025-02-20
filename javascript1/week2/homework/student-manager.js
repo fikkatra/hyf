cd70f1dbdeebe3de157856f3258fbd7d0eec90d7
Sophie Traen <sophie_traen@yahoo.com>
2025-02-20 19:55:37 +0100

    refactor student manager

--- javascript1/week2/homework/student-manager.js
+++ javascript1/week2/homework/student-manager.js
@@ -1,15 +1,18 @@
 const class07Students = [];
 
 function addStudentToClass(studentName) {
-  if (studentName === "") {
-    console.log("You can't add an empty string to a class");
-  } else if (class07Students.length >= 6 && studentName !== "Queen") {
+    console.log("Trying to add student " + studentName + " to the class...");
+  if (class07Students.length >= 6 && studentName !== "Queen") {
     console.log("Cannot add more students to class 07");
+  } else if (studentName === "") {
+    console.log("You can't add an empty string to a class");
   } else if (class07Students.includes(studentName)) {
     console.log("Student " + studentName + " is already in the class");
   } else {
     class07Students.push(studentName);
+    console.log("Student " + studentName + " was added to the class");
   }
+  console.log("The class has " + class07Students.length + " students");
 }
 
 function getNumberOfStudents() {
@@ -18,24 +21,13 @@ function getNumberOfStudents() {
 
 // Test the code
 addStudentToClass("Benjamin");
-console.log(getNumberOfStudents());
 addStudentToClass("Isaac");
-console.log(getNumberOfStudents());
 addStudentToClass("Benjamin"); // Already in the class
-console.log(getNumberOfStudents());
 addStudentToClass("Michelle");
-console.log(getNumberOfStudents());
-addStudentToClass(""); // Empty string
-console.log(getNumberOfStudents());
 addStudentToClass("John");
-console.log(getNumberOfStudents());
+addStudentToClass(""); // Empty string
 addStudentToClass("Anna");
-console.log(getNumberOfStudents());
 addStudentToClass("James");
-console.log(getNumberOfStudents());
 addStudentToClass("Natasha"); // Cannot add more students
-console.log(getNumberOfStudents());
 addStudentToClass("Queen"); // Always add Queen
-console.log(getNumberOfStudents());
 addStudentToClass("Queen"); // Already in the class
-console.log(getNumberOfStudents());
